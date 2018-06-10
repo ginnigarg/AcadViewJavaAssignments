478638ed23d67edd07ba334567beaf603bf5db37
ginnigarg <guneetgarg.2011@gmal.com>
2018-06-10 18:40:21 +0530

    Made Corrections

--- Lect2/PrimeNumber.java
+++ Lect2/PrimeNumber.java
@@ -10,6 +10,10 @@ public class PrimeNumber {
 	}
 	
 	public static boolean checkPrime(int num) {
+		if(num<2)
+		{
+			return false;
+		}
 		for(int i=2;i<=(num/2);i++)
 		{
 			if(num%i==0) {
--- Lect3/Sort.java
+++ Lect3/Sort.java
@@ -1,41 +1,25 @@
 
 public class Sort {
 	
-	public static void sort(int[] arr,int low,int high)
+	sort(int[] arr)
 	{
-		if (low < high)
-        {
-            int pi = partition(arr, low, high);
-            sort(arr, low, pi-1);
-            sort(arr, pi+1, high);
-        }
+		int j=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==0)
+			{
+				int temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+				j++;
+			}
+		}
 	}
-	
-	public static int partition(int arr[], int low, int high)
-    {
-        int pivot = arr[high]; 
-        int i = (low-1);
-        for (int j=low; j<high; j++)
-        {
-            if (arr[j] <= pivot)
-            {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        int temp = arr[i+1];
-        arr[i+1] = arr[high];
-        arr[high] = temp;
- 
-        return i+1;
-    }
 	
 	public static void main(String[] args)
 	{
 		int [] arr = new int[]{1,1,1,1,0,0,1,0};
-		sort(arr,0,arr.length-1);
+		sort(arr);
 		for(int i=0;i<arr.length;i++)
 		{
 			System.out.println(arr[i]);
