b555907b8478f5036c1a10c9875d34b6987eb6d8
ginnigarg <guneetgarg.2011@gmal.com>
2018-06-15 20:19:17 +0530

    Updated Assignment 7

--- Lect7/pattern/Pattern.java
+++ Lect7/pattern/Pattern.java
@@ -7,9 +7,13 @@ public class Pattern {
 		for(int i=0;i<3;i++)
 		{
 			ch = 'a';
+			for(int j=2;j>i;j--)
+			{
+				System.out.print(" ");
+			}
 			for(int j=0;j<=i;j++)
 			{
-				System.out.print(ch);
+				System.out.print(ch+" ");
 				ch++;
 			}
 			System.out.println();
