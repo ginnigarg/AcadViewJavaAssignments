03a0781a5e54bc312afd161f113d35ffe2a305ff
ginnigarg <guneetgarg.2011@gmal.com>
2018-06-22 23:30:54 +0530

    Added Assignment 13

--- Lect13/syncThreads/Task.java
+++ Lect13/syncThreads/Task.java
@@ -5,7 +5,7 @@ public class Task implements Runnable{
 	public synchronized void run() {
 		// TODO Auto-generated method stub
 		int i=1;
-		while(i<1000) {
+		while(i<=1000) {
 			System.out.println(Thread.currentThread().getName()+" "+ i);
 			try {
 				Thread.sleep(10);
